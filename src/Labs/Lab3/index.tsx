336b48e8202d1329a6a70320b7e753dafc2b8a22
Zhiyi Zhang <crystalzzy0609@gmail.com>
2025-05-22 23:18:38 -0700

    before 3.2.4

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,15 @@
+import BooleanVariables from "./BooleanVariables";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+BooleanVariables
+
 export default function Lab3() {
-    return (
-      <div id="wd-lab3">
-        <h2>Lab 3</h2>
-      </div>
-    );
-  }
+  return(
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants/>
+      <VariableTypes />
+      <BooleanVariables />
+    </div>
+  );
+}
