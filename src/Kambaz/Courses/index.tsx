8e60b4e2ed5ae7735bab7a48093e626fc57ede1d
Zhiyi Zhang <crystalzzy0609@gmail.com>
2025-05-26 22:26:31 -0700

    fix course

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,19 +1,23 @@
+import { courses } from "../Database";
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes, useParams} from "react-router";
 import AssignmentEditor from "./Assignments/Editor";
 import { FaAlignJustify } from "react-icons/fa";
 import PeopleTable from "./People/Table";
 
 
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);  
+
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        Course 1234
+        {course && course.name}
       </h2>
       <hr />
       <div className="d-flex">
--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,16 +1,18 @@
 import { Card, Button, Row, Col } from "react-bootstrap";
+import * as db from "./Database";
 import { Link } from "react-router-dom";
 
 export default function Dashboard() {
-  const courses = [
-    { title: "CS1234 React JS", image: "reactjs.jpg" },
-    { title: "CS2345 Node.js", image: "nodejs.jpg" },
-    { title: "CS3456 MongoDB", image: "mongodb.jpg" },
-    { title: "CS4567 Express.js", image: "express.jpg" },
-    { title: "CS5678 HTML & CSS", image: "html.jpg" },
-    { title: "CS6789 JavaScript", image: "javascript.jpg" },
-    { title: "CS7890 AWS Cloud", image: "aws.jpg" },
-  ];
+  const courses = db.courses;
+  // const courses = [
+  //   { title: "CS1234 React JS", image: "reactjs.jpg" },
+  //   { title: "CS2345 Node.js", image: "nodejs.jpg" },
+  //   { title: "CS3456 MongoDB", image: "mongodb.jpg" },
+  //   { title: "CS4567 Express.js", image: "express.jpg" },
+  //   { title: "CS5678 HTML & CSS", image: "html.jpg" },
+  //   { title: "CS6789 JavaScript", image: "javascript.jpg" },
+  //   { title: "CS7890 AWS Cloud", image: "aws.jpg" },
+  // ];
 
   return (
     <div id="wd-dashboard">
@@ -18,25 +20,20 @@ export default function Dashboard() {
       <hr />
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
       <hr />
+      <div id="wd-dashboard-courses">
       <Row xs={1} md={2} lg={4} className="g-4">
-        {courses.map((course, index) => (
-          <Col key={index} className="wd-dashboard-course" style={{ width: "270px" }}>
+        {courses.map((course) => (
+          <Col className="wd-dashboard-course" style={{ width: "270px" }}>
             <Card>
-              <Link
-                to="/Kambaz/Courses/1234/Home"
+              <Link to={`/Kambaz/Courses/${course._id}/Home`}
                 className="wd-dashboard-course-link text-decoration-none text-dark"
               >
-                <Card.Img variant="top" src={`/images/${course.image}`} height={160} />
-                <Card.Body>
+                <Card.Img src={course.image} variant="top" width="100%" height={160} />
+                <Card.Body className="card-body">
                   <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
-                    {course.title}
-                  </Card.Title>
-                  <Card.Text
-                    className="wd-dashboard-course-description overflow-hidden"
-                    style={{ height: "100px" }}
-                  >
-                    Full Stack software developer
-                  </Card.Text>
+                  {course.name} </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                    {course.description} </Card.Text>
                   <Button variant="primary">Go</Button>
                 </Card.Body>
               </Link>
@@ -45,5 +42,4 @@ export default function Dashboard() {
         ))}
       </Row>
     </div>
-  );
-}
+    </div>);}
